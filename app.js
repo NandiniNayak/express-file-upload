e3df8d5a13c184ea4d39397ecfd6b6545d7818e3
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-09-07 18:49:10 +1000

    first commit

---
+++ app.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const multer = require('multer');
+const exphbs = require('express-handlebars');
+const path = require('path');
+const app = express();
+const mongoose = require('mongoose');
+const port = 3000;
+
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
+.then(() => console.log("db connected"))
+.catch((err) => console.log(err));
+
+// set storage Engine
+const storage = multer.diskStorage({
+  destination: './public/uploads',
+  // file name with time stamps, to store a unique name with file extension
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+const upload = multer({
+  storage: storage,
+  limits: {fileSize: 1000000},
+  fileFilter: (req, file, cb) => {
+    checkFileType(file, cb);
+  }
+}).array('myImage', 10); //for single files, use arrays for multiple files
+
+// check file type
+const checkFileType = (file, cb) => {
+  // Allowed ext
+  const fileTypes = /jpeg|jpg|png|gif/;
+  // check extension
+  const extname = fileTypes.test(path.extname(file.originalname).toLowerCase());
+  // check mime type
+
+  const mimetype = fileTypes.test(file.mimetype);
+
+  if(mimetype && extname) {
+    return cb(null, true);
+  } else {
+    cb('Error: Images Only')
+  }
+}
+
+require('./model/blog');
+const Blog = mongoose.model('blogs');
+// middle ware start
+app.use(express.static('public'));
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// middleware end
+app.get('/', (req, res) => {
+    res.render('home');
+});
+
+app.post('/upload', (req, res) => {
+
+  upload(req, res, (err) => {
+    if (err) {
+      console.log('error from upload fucntion')
+      res.render('home', {
+        msg: err
+      });
+    } else {
+      // res.send('test');
+      if(req.files == undefined) {
+        res.render('home', {
+          msg: 'Error: No file selected'
+        });
+      } else {
+        // console.log(req.files[0].path)
+        res.render('home',{
+          msg: 'File Uploaded',
+          // files: `uploads/${req.file.filename}`
+          files: req.files
+        });
+      }
+    }
+  });
+});
+
+app.listen(port, () => console.log(`server started on port ${port}`));
---
+++ public/javascript/home.js
@@ -0,0 +1,6 @@
+  // $(document).ready(function(){
+  //     var filename = document.getElementById('custId').value;
+  //     url = `./uploads/${filename}`;
+  //     console.log(url);
+  //     $( ".show-img" ).append(`<img class="responsive-img" src=${url} alt="blah">`);
+  // });
