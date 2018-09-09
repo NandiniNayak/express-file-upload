d1a1a6e78e1e33cbf073a0ebe234b7fe230671bf
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-09-09 16:17:16 +1000

    save fiels to database

--- public/javascript/home.js
+++ public/javascript/home.js
@@ -4,3 +4,24 @@
   //     console.log(url);
   //     $( ".show-img" ).append(`<img class="responsive-img" src=${url} alt="blah">`);
   // });
+
+
+// loop within the loop
+
+// To be debugged: loop through the blogs and for each blog loops through the files and fetch filename to display the image
+// const loopFiles = (myDoc) =>{
+//    myDoc.forEach(return showFileName(file))
+// }
+//
+// const showFileName = (file) => {
+//   file.forEach(return filename)
+// }
+// const filename = db.blogs.find().forEach(loopFiles(myDoc))
+//
+//
+//
+// db.blogs.find().forEach(function(blog) {
+//   blog.files.forEach(function(file) {
+//     return (file.filename)
+//   })
+// })
