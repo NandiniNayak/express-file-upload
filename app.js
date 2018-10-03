a3c4879f8f31295971586ff1ec43f65d7a0828a0
Nandini Nayak <nandini.r.nayak@gmail.com>
2018-10-03 12:45:50 +1000

    title and caption included

--- app.js
+++ app.js
@@ -1,31 +1,46 @@
-const express = require('express');
-const multer = require('multer');
-const exphbs = require('express-handlebars');
-const path = require('path');
+const express = require("express");
 const app = express();
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+// const multiparty = require("multiparty");
+const multer = require("multer");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const mongoose = require("mongoose");
+
 const port = 3000;
 
-mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true })
-.then(() => console.log("db connected"))
-.catch((err) => console.log(err));
+// parse application/json
+app.use(bodyParser.json());
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose
+  .connect(
+    "mongodb://localhost/test",
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log("db connected"))
+  .catch(err => console.log(err));
 
 // set storage Engine
 const storage = multer.diskStorage({
-  destination: './public/uploads',
+  destination: "./public/uploads",
   // file name with time stamps, to store a unique name with file extension
   filename: (req, file, cb) => {
-    cb(null, file.fieldname + '_' + Date.now() + path.extname(file.originalname));
+    cb(
+      null,
+      file.fieldname + "_" + Date.now() + path.extname(file.originalname)
+    );
   }
 });
 
 const upload = multer({
   storage: storage,
-  limits: {fileSize: 1000000},
+  limits: { fileSize: 1000000 },
   fileFilter: (req, file, cb) => {
     checkFileType(file, cb);
   }
-}).array('myImage', 10); //for single files, use arrays for multiple files
+}).array("myImage", 10); //for single files, use arrays for multiple files
 
 // check file type
 const checkFileType = (file, cb) => {
@@ -37,74 +52,76 @@ const checkFileType = (file, cb) => {
 
   const mimetype = fileTypes.test(file.mimetype);
 
-  if(mimetype && extname) {
+  if (mimetype && extname) {
     return cb(null, true);
   } else {
-    cb('Error: Images Only')
+    cb("Error: Images Only");
   }
-}
+};
 
-require('./model/blog');
-const Blog = mongoose.model('blogs');
+require("./model/blog");
+const Blog = mongoose.model("blogs");
 // middle ware start
-app.use(express.static('public'));
-app.engine('handlebars', exphbs({defaultLayout: 'main'}));
-app.set('view engine', 'handlebars');
+app.use(express.static("public"));
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
 // middleware end
-app.get('/', (req, res) => {
-    // Blog.find()
-    // .then(blogs => {
-    //   console.log('found blogs')
-    //   console.log(blogs)
-    // })
-    // res.render('home',{
-    //   files: blogs
-    // });
-    res.render('home');
+app.get("/", (req, res) => {
+  // Blog.find()
+  // .then(blogs => {
+  //   console.log('found blogs')
+  //   console.log(blogs)
+  // })
+  // res.render('home',{
+  //   files: blogs
+  // });
+  res.render("home");
 });
 
-app.post('/upload', (req, res) => {
-
-  upload(req, res, (err) => {
+app.post("/upload", (req, res) => {
+  console.log(req.body);
+  upload(req, res, err => {
     if (err) {
-      console.log('error from upload fucntion')
-      res.render('home', {
+      console.log("error from upload fucntion");
+      res.render("home", {
         msg: err
       });
     } else {
       // res.send('test');
-      if(req.files == undefined) {
-        res.render('home', {
-          msg: 'Error: No file selected'
+      if (req.files == undefined) {
+        res.render("home", {
+          msg: "Error: No file selected"
         });
       } else {
         // save to database
+        console.log("in post route");
+        console.log(req.body);
         let newBlog = {
+          title: req.body.title,
+          caption: req.body.caption,
           files: req.files
-        }
-        new Blog(newBlog)
-        .save()
-        .then (blogs => {
-          console.log('posted blogs')
+        };
+        new Blog(newBlog).save().then(blogs => {
+          console.log("posted blogs");
           // console.log(blogs);
-          res.redirect('/upload');
-        })
+          res.redirect("/upload");
+        });
       }
     }
   });
 });
 // fetch all the posts uploaded
-app.get('/upload', (req, res) => {
+app.get("/upload", (req, res) => {
   Blog.find()
-   .then(blogs => {
-     console.log('found blogs');
-     console.log(blogs);
-     res.render('home', {
-       blogs: blogs
-     });
-   })
-   .catch(err => console.log(err));
+    .then(blogs => {
+      console.log("found blogs");
+      console.log(blogs);
+      res.render("home", {
+        blogs: blogs
+      });
+    })
+    .catch(err => console.log(err));
 });
 
 app.listen(port, () => console.log(`server started on port ${port}`));
--- model/Blog.js
+++ model/Blog.js
@@ -1,15 +1,15 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const BlogSchema = new Schema({
-   // array of objects which is of mixed datatype
-   // https://mongoosejs.com/docs/schematypes.html#arrays
+  // array of objects which is of mixed datatype
+  // https://mongoosejs.com/docs/schematypes.html#arrays
+  title: String,
+  caption: String,
   files: [Schema.Types.Mixed]
-})
+});
 
-mongoose.model('blogs', BlogSchema);
-
-
+mongoose.model("blogs", BlogSchema);
 
 // Note:
 // Now Mongoose supports subdocuments, which are the documented way to nest arrays,
